b41051328f278a2322476a71fe50f75a635fc0fb
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2024-07-10 11:50:55 +0100

    test: ensure LoadUploadFileById throw if loadUploadFileRepository throws

--- tests/core/usecases/load-upload-file-by-id.spec.ts
+++ tests/core/usecases/load-upload-file-by-id.spec.ts
@@ -12,8 +12,15 @@ const makeSut = () => {
 
 describe('LoadUploadFileById', () => {
   it('Should call loadUploadFileByIdRepository with correct param', async () => {
-   const { loadUploadFileByIdRepository, sut} = makeSut();
+    const {loadUploadFileByIdRepository, sut} = makeSut();
     await sut('any_id');
     expect(loadUploadFileByIdRepository).toHaveBeenCalledWith('any_id');
+  });
+
+  it('Should throw if loadUploadFileByIdRepository throws', async () => {
+    const {loadUploadFileByIdRepository, sut} = makeSut();
+    loadUploadFileByIdRepository.mockRejectedValue(new Error());
+    const promise = sut('any_id');
+    await expect(promise).rejects.toThrow(new Error());
   })
 });
