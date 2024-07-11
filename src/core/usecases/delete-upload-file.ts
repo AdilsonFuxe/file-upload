99ffa1fe3d1f2b1d3dbef2d6ab2c51176776b7db
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2024-07-11 15:20:45 +0100

    test: ensure deleteUploadById  not call deleteLocalFile and deleteUploadFileByIdRepository if upload not found

--- src/core/usecases/delete-upload-file.ts
+++ src/core/usecases/delete-upload-file.ts
@@ -1,11 +1,24 @@
-import {DeleteUploadFileByIdRepository} from "../../ports/out";
+import {DeleteUploadFileByIdRepository, DeleteLocalFile, LoadUploadFileByIdRepository} from "../../ports/out";
 import {DeleteUploadFileById} from "../../ports/in";
 
 type Dependencies = {
   deleteUploadFileByIdRepository: DeleteUploadFileByIdRepository
+  loadUploadFileByIdRepository: LoadUploadFileByIdRepository,
+  deleteLocalFile: DeleteLocalFile;
 }
 
-type BuildDeleteUploadFileById = (dependencies:Dependencies) => DeleteUploadFileById;
-export const deleteUploadFileById: BuildDeleteUploadFileById = ({deleteUploadFileByIdRepository}) => async (id: string) => {
-  await deleteUploadFileByIdRepository(id)
+type BuildDeleteUploadFileById = (dependencies: Dependencies) => DeleteUploadFileById;
+export const deleteUploadFileById: BuildDeleteUploadFileById = ({
+                                                                  deleteUploadFileByIdRepository,
+                                                                  loadUploadFileByIdRepository,
+                                                                  deleteLocalFile
+                                                                }) => async (id: string) => {
+
+  const upload = await loadUploadFileByIdRepository(id);
+
+  if (upload) {
+    await deleteUploadFileByIdRepository(id)
+    await deleteLocalFile(upload.key);
+  }
+
 }
--- tests/core/usecases/delete-file.spec.ts
+++ tests/core/usecases/delete-file.spec.ts
@@ -1,15 +1,41 @@
 import {deleteUploadFileById} from "../../../src/core/usecases";
+import {Upload} from "../../../src/core/models";
 
 
 const makeSut = () => {
   const deleteUploadFileByIdRepository = jest.fn();
-  const sut = deleteUploadFileById({deleteUploadFileByIdRepository});
+  const deleteLocalFile = jest.fn();
+  const mockedUpload = (): Upload => ({
+    id: 'any_id',
+    name: 'any_name',
+    mimeType: 'any_mime',
+    size: 0.3,
+    key: 'any_key',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  const loadUploadFileByIdRepository = jest.fn().mockResolvedValue(mockedUpload());
+  const sut = deleteUploadFileById({deleteUploadFileByIdRepository, deleteLocalFile,loadUploadFileByIdRepository });
   return {
-    sut, deleteUploadFileByIdRepository
+    sut, deleteUploadFileByIdRepository, loadUploadFileByIdRepository, deleteLocalFile, mockedUpload
   }
 }
 
 describe('Delete File', () => {
+
+  it('Should call loadUploadFileByIdRepository with correct param', async () => {
+    const {loadUploadFileByIdRepository, sut} = makeSut();
+    await sut('any_id');
+    expect(loadUploadFileByIdRepository).toHaveBeenCalledWith('any_id');
+  });
+
+  it('Should throw if loadUploadFileByIdRepository throws', async () => {
+    const {loadUploadFileByIdRepository, sut} = makeSut();
+    loadUploadFileByIdRepository.mockRejectedValue(new Error());
+    const promise = sut('any_id');
+    await expect(promise).rejects.toThrow(new Error());
+  });
+
   it('Should call deleteUploadFileByIdRepository with correct params', async () => {
     const {sut, deleteUploadFileByIdRepository} = makeSut();
     await sut('any_id');
@@ -21,5 +47,28 @@ describe('Delete File', () => {
     deleteUploadFileByIdRepository.mockRejectedValue(new Error());
     const promise = sut('any_id');
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  it('Should call deleteLocalFile with correct param', async () => {
+    const {deleteLocalFile, sut, mockedUpload} = makeSut();
+    await sut('any_id');
+    const upload = mockedUpload();
+    expect(deleteLocalFile).toHaveBeenCalledWith(upload.key);
+  });
+
+  it('Should throw if deleteLocalFile throws', async () => {
+    const {deleteLocalFile, sut} = makeSut();
+    deleteLocalFile.mockRejectedValue(new Error());
+    const promise = sut('any_id');
+    await expect(promise).rejects.toThrow(new Error());
+  });
+
+  it('Should not call deleteLocalFile and deleteUploadFileByIdRepository if upload not found', async () => {
+    const {deleteLocalFile, sut, loadUploadFileByIdRepository, deleteUploadFileByIdRepository, mockedUpload} = makeSut();
+    loadUploadFileByIdRepository.mockResolvedValue(null);
+    await sut('any_id');
+
+    expect(deleteUploadFileByIdRepository).toHaveBeenCalledTimes(0);
+    expect(deleteLocalFile).toHaveBeenCalledTimes(0);
   })
 })
